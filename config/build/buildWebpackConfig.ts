ba5a868d89df93149ca6ccec54cf0fc8251cd6b5
Stariu <v.starynets0013@gmail.com>
2025-02-09 14:00:32 +0000

    add dev server

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -3,9 +3,10 @@ import buildLoaders from "./buildLoaders";
 import buildResolvers from "./buildResolvers";
 import webpack from "webpack";
 import {BuildOptions} from "./types/config";
+import buildDevServer from "./buildDevServer";
 
 export default function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
-    const { mode, paths } = options;
+    const { mode, paths, isDev } = options;
 
     return {
         mode,
@@ -20,5 +21,8 @@ export default function buildWebpackConfig(options: BuildOptions): webpack.Confi
             rules: buildLoaders(),
         },
         resolve: buildResolvers(),
+        // while mode dev there will be sourceMap in build files(which are not needed for prod)
+        devServer: isDev ? buildDevServer(options) : void 0,
+        devtool: isDev ? 'inline-source-map' : void 0,
     }
 }
